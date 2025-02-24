5a397f5637b908568d87de93b87a4d778383c9f4
HeyLookAtThis <r.a.yamov@yandex.ru>
2025-02-24 09:01:48 +0500

    Fixed(09.01)

--- Assets/Scripts/Launcher/Launcher.cs
+++ Assets/Scripts/Launcher/Launcher.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 using UnityEngine;
 
 public class Launcher : MonoBehaviourPunCallbacks
@@ -9,41 +10,43 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject _controlPanel;
     [SerializeField] private GameObject _progressLabel;
 
-    private string _gameVersion = "1";
     private bool _isConnecting;
+
+    private Coroutine _connectingWaiter;
 
     private void Awake() => PhotonNetwork.AutomaticallySyncScene = true;
 
     private void Start()
     {
-        _progressLabel.SetActive(false);
-        _controlPanel.SetActive(true);
-
-        _isConnecting = PhotonNetwork.ConnectUsingSettings();
-        PhotonNetwork.GameVersion = _gameVersion;
+        ShowProgressPanel();
+        TryConnected();
     }
 
     public override void OnConnectedToMaster()
     {
+        if (_connectingWaiter != null)
+        {
+            StopCoroutine(_connectingWaiter);
+            HideProgressPanel();
+        }
+
         if (_isConnecting)
         {
             Debug.Log("Launcher: OnConnectedToMaster() was called by PUN");
-            PhotonNetwork.JoinLobby();
             _isConnecting = false;
         }
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        _progressLabel.SetActive(false);
-        _controlPanel.SetActive(true);
-
+        HideProgressPanel();
         Debug.LogWarningFormat("Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.LogWarningFormat("Launcher:OnJoinRandomFailed() was called by PUN. No random room available");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = MaxPlayersPerRoom });
     }
 
     public override void OnJoinedRoom()
@@ -51,41 +54,59 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.LoadLevel(Scenes.Room);
     }
 
-    public override void OnCreateRoomFailed(short returnCode, string message)
+    public void Connect()
     {
-        base.OnCreateRoomFailed(returnCode, message);
-    }
-
-    public void Connect(string roomName)
-    {
-        _progressLabel.SetActive(true);
-        _controlPanel.SetActive(false);
-
-        if (PhotonNetwork.IsConnected)
-        {
-            PhotonNetwork.JoinRoom(roomName);
-        }
-        else
-        {
-            _isConnecting = PhotonNetwork.ConnectUsingSettings();
-            PhotonNetwork.GameVersion = _gameVersion;
-        }
+        ShowProgressPanel();
+        TryConnected();
+        PhotonNetwork.JoinRandomRoom();
     }
 
     public void Create(string roomName)
     {
-        _progressLabel.SetActive(true);
-        _controlPanel.SetActive(false);
+        ShowProgressPanel();
+        TryConnected();
+        PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = MaxPlayersPerRoom });
+    }
 
-        if (PhotonNetwork.IsConnected)
-        {
-            PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = MaxPlayersPerRoom });
-            Debug.Log("Launcher: Create");
-        }
-        else
+    private void TryConnected()
+    {
+        if (PhotonNetwork.IsConnected == false)
         {
             _isConnecting = PhotonNetwork.ConnectUsingSettings();
-            PhotonNetwork.GameVersion = _gameVersion;
+            _connectingWaiter = StartCoroutine(ConnectingWaiter());
         }
+    }
+
+    private IEnumerator ConnectingWaiter()
+    {
+        float seconds = 5f;
+        var waitTime = new WaitForEndOfFrame();
+
+        float passedTime = 0;
+
+        while(passedTime < seconds)
+        {
+            passedTime += Time.deltaTime;
+            Debug.Log(passedTime);
+            yield return waitTime;
+        }
+
+        if (passedTime >= seconds)
+        {
+            HideProgressPanel();
+            yield break;
+        }
+    }
+
+    private void ShowProgressPanel()
+    {
+        _progressLabel.SetActive(true);
+        _controlPanel.SetActive(false);
+    }
+
+    private void HideProgressPanel()
+    {
+        _progressLabel.SetActive(false);
+        _controlPanel.SetActive(true);
     }
 }
